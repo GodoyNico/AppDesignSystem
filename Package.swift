ebe263223bc240734a5466986ca0bc6668390ae0
Nícolas Godoy de Almeida <Godoy.nico@icloud.com>
2025-01-29 21:36:03 -0300

    fix: setup lib

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AppDesignSystem",
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,11 +15,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AppDesignSystem"),
-        .testTarget(
-            name: "AppDesignSystemTests",
-            dependencies: ["AppDesignSystem"]
-        ),
-    ]
+            .target(
+                name: "AppDesignSystem",
+                dependencies: []
+            ),
+            .testTarget(
+                name: "AppDesignSystemTests",
+                dependencies: ["AppDesignSystem"]
+            ),
+        ]
 )
